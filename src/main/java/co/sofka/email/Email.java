8a849d22ed77f4b7b1747f5d23be781015fa18b0
jcelisca <celiss1997@gmail.com>
2022-03-22 11:10:52 -0500

    Se crean los filtros por dominio y el conteo de correos

--- src/main/java/co/sofka/email/Email.java
+++ src/main/java/co/sofka/email/Email.java
@@ -1,10 +1,14 @@
 package co.sofka.email;
 
+import java.util.Objects;
+
 public class Email {
+    private Integer id;
     private String email;
     private String estado;
 
-    public Email(String email, String estado) {
+    public Email(Integer id, String email, String estado) {
+        this.id = id;
         this.email = email;
         this.estado = estado;
     }
@@ -23,5 +27,35 @@ public class Email {
 
     public void setEstado(String estado) {
         this.estado = estado;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email1 = (Email) o;
+        return email.equals(email1.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
     }
 }
---
+++ src/main/java/co/sofka/email/Main.java
@@ -0,0 +1,78 @@
+package co.sofka.email;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Email> listaEmails = new ArrayList<>();
+        listaEmails.add(new Email(1, "pepito@gmail.com", "Enviado"));
+        listaEmails.add(new Email(2, "anita@gmail.com", "No Enviado"));
+        listaEmails.add(new Email(3, "jesus@hotmail.com", "Enviado"));
+        listaEmails.add(new Email(4, "mariana@nocumplo.cm", "No Enviado"));
+        listaEmails.add(new Email(5, "maria@gmail.com", "No Enviado"));
+        listaEmails.add(new Email(6, "pep@nocumplo.cm", "No Enviado"));
+        listaEmails.add(new Email(7, "poo@gmail.com", "Enviado"));
+        listaEmails.add(new Email(8, "pepita@gmail.com", "No Enviado"));
+        listaEmails.add(new Email(9, "pepit@gmail.com", "Enviado"));
+        listaEmails.add(new Email(10, "pepo@gmail.com", "Enviado"));
+        listaEmails.add(new Email(11, "peto@hotmail.com", "No Enviado"));
+        listaEmails.add(new Email(12, "pepito@outlook.com", "No Enviado"));
+        listaEmails.add(new Email(13, "pepito@nocumplo.cm", "No Enviado"));
+        listaEmails.add(new Email(14, "carlos@hotmail.com", "Enviado"));
+        listaEmails.add(new Email(15, "plie@gmail.com", "Enviado"));
+        listaEmails.add(new Email(16, "woa@outlook.com", "No Enviado"));
+        listaEmails.add(new Email(17, "link@gmail.com", "No Enviado"));
+        listaEmails.add(new Email(18, "zelda@outlook.com", "No Enviado"));
+        listaEmails.add(new Email(29, "skywalker@gmail.com", "Enviado"));
+        listaEmails.add(new Email(20, "vader@hotmail.com", "Enviado"));
+        listaEmails.add(new Email(21, "kyloren@gmail.com", "Enviado"));
+        listaEmails.add(new Email(22, "leiaprincess@hotmail.com", "Enviado"));
+        listaEmails.add(new Email(23, "jedi@gmail.com", "Enviado"));
+        listaEmails.add(new Email(24, "link@gmail.com", "Enviado"));
+        listaEmails.add(new Email(25, "pepito@gmail.com", "Enviado"));
+        listaEmails.add(new Email(26, "pepito@nocumplo.cm", "No Enviado"));
+        listaEmails.add(new Email(27, "pepito@gmail.com", "Enviado"));
+        listaEmails.add(new Email(28, "pepito@gmail.com", "No Enviado"));
+        listaEmails.add(new Email(29, "pepito@hotmail.com", "No Enviado"));
+        listaEmails.add(new Email(30, "pepito@gmail.com", "Enviado"));
+
+
+//Flux para correos que no se repiten usando distinct
+        /*Flux<Email> distinct = Flux.fromIterable(listaEmails)
+                .distinct();
+
+        distinct.subscribe(System.out::println);*/
+
+//-----------------------------------------------------------------
+        //Correos con gmail
+        Flux<Email> gmail = Flux.fromIterable(listaEmails)
+                .filter(email -> email.getEmail().contains("@gmail.com"));
+
+     //   gmail.subscribe(System.out::println);
+
+        //Correos con hotmail
+        Flux<Email> hotmail = Flux.fromIterable(listaEmails)
+                .filter(email -> email.getEmail().contains("@hotmail.com"));
+//        hotmail.subscribe(System.out::println);
+
+
+        //Correos con outlook
+        Flux<Email> outlook = Flux.fromIterable(listaEmails)
+                .filter(email -> email.getEmail().contains("@outlook.com"));
+
+       // outlook.subscribe(System.out::println);
+
+        Mono<Long> count = Flux.merge(gmail,hotmail,outlook)
+                .count();
+
+        System.out.println("El total de correos correctos: ");
+        count.subscribe(System.out::println);
+    }
+
+
+}
