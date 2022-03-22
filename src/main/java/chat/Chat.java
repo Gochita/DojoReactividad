d1a7091f1b321401801b187764c0fec07b92a0cf
tamagochita <mevere66@gmail.com>
2022-03-22 12:15:42 -0500

    Para pracitcar, ya se hizo la entrega, estoy es practicando xD, mirando lo del chat

---
+++ src/main/java/chat/Chat.java
@@ -0,0 +1,12 @@
+package chat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chat {
+    public static void main(String[] args) {
+        List<String> malasPalabras= new ArrayList<>();
+
+    }
+
+}
